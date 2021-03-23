0bac7509bc2caea0da5ddac482fb50f4b505e8aa
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-03-22 17:52:34 -0700

    Delete www.amazon.com entry from history
    Prevents loop where user hits the back button, but the previous history entry is www.amazon.com, causing the extension to redirect back to smile.amazon.com again

--- chrome/background.js
+++ chrome/background.js
@@ -1,14 +1,25 @@
-//Copyright (C) 2019 Imran Majeed
+//Copyright (C) 2021 Imran Majeed
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.tabs.get(tabId, function(tab){
+chrome.tabs.onUpdated.addListener((tabId) => {
+    chrome.tabs.get(tabId, (tab) => {
         if (tab.url.includes("www.amazon.com")) {
-            chrome.tabs.update(
-                tabId,
-                {
-                    url: tab.url.replace("www.", "smile.")
-                }
-            );
+            // get timestamp of visit to www.amazon.com ...
+            chrome.history.search({ text: "www.amazon.com" }, (results) => {
+                // ...then delete the www.amazon.com entry from history
+                // so that pressing the back button will not go back to www.amazon.com
+                // (that would  cause a redirect back to smile.amazon.com, creating a loop)
+                chrome.history.deleteRange({
+                    startTime: results[0].lastVisitTime - 0.001,
+                    endTime: results[0].lastVisitTime + 0.001,
+                }, () => {
+                    chrome.tabs.update(
+                        tabId,
+                        {
+                            url: tab.url.replace("www.", "smile.")
+                        }
+                    );
+                });
+            });
         }
-    });    
+    });
 });
